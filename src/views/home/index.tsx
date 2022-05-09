6af701e0b50906ee2c262f687be7cf7ceafaa79e
Sandy McFadden <sandy.mcfadden@automattic.com>
2022-05-08 21:30:44 -0300

    Fix home route

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,24 +1,31 @@
 import { Container } from "@mui/material";
-import { Redirect } from "wouter";
+import { Game } from "../../components/game";
 import { MenuAppBar } from "../../components/menuAppBar";
+import { Season } from "../../components/seasons/Season";
+import { SeasonList } from "../../components/seasons/SeasonList";
 import { useGames } from "../../hooks/data/game";
 import { useSeasons } from "../../hooks/data/season";
 
 export const Home = () => {
   const { seasons } = useSeasons();
   const { games } = useGames();
-  console.log(games);
+
+  const seasonId =
+    games.length > 0
+      ? games[0].seasonId
+      : seasons.length > 0
+      ? seasons[0].id || ""
+      : "";
+
+  const gameId = games.length > 0 ? games[0].id || "" : "";
 
   return (
     <>
-      {games.length > 0 && (
-        <Redirect to={`/season/${games[0].seasonId}/game/${games[0].id}`} />
-      )}
-      {/*seasons.length > 0 && <Redirect to={`/season/${seasons[0].id}`} />*/}
-      {/*seasons.length <= 0 && <Redirect to="/seasons" />*/}
       <MenuAppBar />
       <Container maxWidth="lg" sx={{ mt: 3 }}>
-        <h1>Loading...</h1>
+        {gameId && <Game seasonId={seasonId} gameId={gameId} />}
+        {!gameId && seasonId && <Season seasonId={seasonId} />}
+        {!gameId && !seasonId && <SeasonList />}
       </Container>
     </>
   );
